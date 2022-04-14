f7849c386e7dc2385ab08b66bc0e985c5a8b39dc
Alexander Myagkov <anistonbale@gmail.com>
2022-04-14 16:58:18 +0300

    Reshenie zada4a 1

--- seminar1/Seminar001/Program.cs
+++ seminar1/Seminar001/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA*numberA==numberB)
+{
+    Console.WriteLine("Yes");
+}
+else Console.WriteLine("No!!");
