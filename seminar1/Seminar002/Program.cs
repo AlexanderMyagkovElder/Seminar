47b07e51235f9abee276be8f5a719e411e799625
Alexander Myagkov <anistonbale@gmail.com>
2022-04-14 17:21:10 +0300

    Reshenie zada4a2 days of week

--- seminar1/Seminar002/Program.cs
+++ seminar1/Seminar002/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.Write("Введите номер дня недели ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 1) Console.WriteLine("Monday");
+if (a == 2) Console.WriteLine("Tuesday");
+if (a == 3) Console.WriteLine("Wednesday");
+if (a == 4) Console.WriteLine("Thursday");
+if (a == 5) Console.WriteLine("Friday");
+if (a == 6) Console.WriteLine("Saturday");
+if (a == 7) Console.WriteLine("Sunday");
+if (a >7 ||a<=0) Console.WriteLine("Only 7 day on a week!!!");
