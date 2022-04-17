84e4577742e63240dc8206a78eced714ce2bc032
Alexander Myagkov <anistonbale@gmail.com>
2022-04-17 21:11:35 +0300

    reshenie zada4a 3 Cratos/NeCratos

--- seminar1/DZ/03/Program.cs
+++ seminar1/DZ/03/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным.
+Console.Write("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число " + a + " кратное");
+}
+else
+{
+    Console.WriteLine("Число " + a + " некратное");
+}
