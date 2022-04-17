ab76d711f2a892cf9866be7239ceec0ea8fb959b
Alexander Myagkov <anistonbale@gmail.com>
2022-04-17 20:31:37 +0300

    reshenie zadacha1 Max number

--- seminar1/DZ/01/Program.cs
+++ seminar1/DZ/01/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает 2 числа и выдает, какое число большее, а какое меньшее
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max) Console.WriteLine("Большее число: " + b + "  Меньшее число: " + a);
+else Console.Write("Большее число: " + a + "  Меньшее число: " + b);
