d7a1096ea27b14f38fd4b6978e9e45c548365775
Alexander Myagkov <anistonbale@gmail.com>
2022-04-17 21:26:53 +0300

    reshenie zadacha4 about Cratos N

--- seminar1/DZ/04/Program.cs
+++ seminar1/DZ/04/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int n = 1;
+while (n <= N)
+{
+    if (n % 2 == 0)
+    {
+        Console.Write(n + ", ");
+        n++;
+    }
+    else n++;
+}
