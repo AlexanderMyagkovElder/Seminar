f9d8b271b36fcd47ff87db2798a99ff11ba35fab
Alexander Myagkov <anistonbale@gmail.com>
2022-04-24 19:39:14 +0300

    Reshenie zadachi3 pro cratos/necratos

--- seminar2/02/Program.cs
+++ seminar2/02/Program.cs
@@ -1,7 +1,13 @@
 ﻿//выводит 3хзначное число и удаляет вторую цифру этого числа
 int number = new Random().Next(100,1000);
-Console.WriteLine(number);
-int chislo = Sotni(number);
+string [] array = Convert.ToString(number);
+Console.WriteLine(number(0)+number(2));
+
+
+
+
+
+/*int chislo = Sotni(number);
 int Sotni(int number)
 {
     int chislo = number / 100;
@@ -15,4 +21,6 @@ int Edinicy(int number)
     return chislo2;
 }
 int newchislo = chislo*10 + chislo2;
-Console.WriteLine("Ваше числo: " + newchislo);
+
+
+Console.WriteLine("Ваше числo: " + newchislo);*/
---
+++ seminar2/03/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число
+кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деление*/
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int ostatok = Ostatok(number2, number1);
+int Ostatok(int number2, int number1)
+{
+    ostatok = number2 % number1;
+    return ostatok;
+}
+
+if (number2 % number1 == 0)
+{
+    Console.WriteLine("Число кратно");
+}
+else
+{
+    Console.WriteLine("Число некратно. Остаток от деления: " + ostatok);
+}
+
