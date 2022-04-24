0f0ba1f2e4427c226a5ae5ffb5885adc78cb7657
Alexander Myagkov <anistonbale@gmail.com>
2022-04-24 18:48:19 +0300

    Add zadacha 2 pro 456 & 46

---
+++ seminar2/02/Program.cs
@@ -0,0 +1,18 @@
+﻿//выводит 3хзначное число и удаляет вторую цифру этого числа
+int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+int chislo = Sotni(number);
+int Sotni(int number)
+{
+    int chislo = number / 100;
+    return chislo;
+}
+
+int chislo2 = Edinicy(number);
+int Edinicy(int number)
+{
+    int chislo2 = number % 10;
+    return chislo2;
+}
+int newchislo = chislo*10 + chislo2;
+Console.WriteLine("Ваше числo: " + newchislo);
