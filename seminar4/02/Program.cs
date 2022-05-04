f93f55971b4531f8763291905df95c1f8340eed9
Alexander Myagkov <anistonbale@gmail.com>
2022-05-04 16:38:26 +0300

    add 873 -> 3 цифры

---
+++ seminar4/02/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5*/
+Console.Write("Введите число: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+int kolvo = 0;
+string number = Convert.ToString(chislo);
+for (int i = 1; i <= number.Length; i++)
+{
+    kolvo += 1;
+}
+Console.WriteLine($"Количество цифр равно {kolvo}");
