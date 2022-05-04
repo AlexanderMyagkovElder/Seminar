7d08c1347d56d352d7bfdab645e64c2612614bcf
Alexander Myagkov <anistonbale@gmail.com>
2022-05-04 18:38:06 +0300

    01101101

--- seminar4/03/Program.cs
+++ seminar4/03/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿/* Напишите программу, которая выводит массив из 8 элементов, 
+заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0] */
+int [] massive = new int[8];
+for (int i = 0; i < massive.Length; i++)
+{
+    massive[i] = new Random().Next(0, 2);
+    Console.Write(massive[i]+" ");
+}
