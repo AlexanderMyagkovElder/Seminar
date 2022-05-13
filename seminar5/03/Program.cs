d90a7a63c9e36ba1d5cb8a4579626d7f4aef4891
Alexander Myagkov <anistonbale@gmail.com>
2022-05-13 11:16:05 +0300

    zadacha pro poisk

---
+++ seminar5/03/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 35: Задайте одномерный массив из 123 случайных чисел. 
+Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. */
+int [] massive = new int[123];
+Zapolnenie(massive);
+int sum = 0;
+for (int i = 0; i < massive.Length; i++)
+{
+    if (massive[i] < 100 && massive[i] > 9)
+    {
+        sum += 1;
+    }
+}
+Console.WriteLine(sum);
+
+void Zapolnenie(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        massive[i] = new Random().Next(0, 1000);
+        Console.Write(massive[i]+" ");
+    }
+    Console.WriteLine();
+}
