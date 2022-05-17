c74921c2c118b3c087b716178d45d035ca01f2c3
Alexander Myagkov <anistonbale@gmail.com>
2022-05-17 23:52:22 +0300

    zapolnenie dwumernogo massiva

---
+++ seminar7/03/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1 */
+Console.Write("введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m,n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i,j]=new Random().Next(-10,10);
+    }
+}
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(array[i,j]+" ");
+    }
+    Console.WriteLine();
+}
